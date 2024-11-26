c6a70c23fa67822d54ae449c0d92890a29e89243
PrinceLeomarSobrepena <princeeeleomar0317@gmail.com>
2024-11-26 08:41:18 +0800

    css

--- middleware/middleware.js
+++ middleware/middleware.js
@@ -1,7 +1,6 @@
 module.exports = (req, res, next) => {
-  if (!req.session.isAdmin) {
-    return res.redirect('/login');
+  if (!req.session || !req.session.isAdmin) {
+    return res.redirect('/login'); // Redirect to login if not authenticated as admin
   }
-  next();
+  next(); // Proceed to the next middleware or route handler
 };
-
