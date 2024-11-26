38a263c43cfcda60e2153d251a1015104a85752a
Danikuuu <danikuroque@gmail.com>
2024-11-26 10:26:42 +0800

    changes

--- middleware/middleware.js
+++ middleware/middleware.js
@@ -1,6 +1,17 @@
-module.exports = (req, res, next) => {
-  if (!req.session || !req.session.isAdmin) {
-    return res.redirect('/login'); // Redirect to login if not authenticated as admin
+//module.exports = (req, res, next) => {
+//  if (!req.session || !req.session.isAdmin) {
+//    return res.redirect('/login'); // Redirect to login if not authenticated as admin
+//  }
+//  next(); // Proceed to the next middleware or route handler
+//};
+
+// Middleware for authentication
+const isAuthenticated = (req, res, next) => {
+  if (req.session && req.session.isAdmin) {
+    return next(); // Proceed if user is authenticated
   }
-  next(); // Proceed to the next middleware or route handler
+  res.redirect('/login'); // Redirect if not authenticated
 };
+
+module.exports = isAuthenticated;
+
